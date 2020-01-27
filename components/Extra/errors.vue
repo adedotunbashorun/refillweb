23552eeb76d878cddbdbd63147e475a0e6549448
adedotunbashorun <adedotunolawale@gmail.com>
2020-01-27 19:28:27 +0100

    minor fix

---
+++ components/Extra/errors.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div v-if="errors.length">
+      <b>Please correct the following error(s):</b>
+      <div class="alert alert-danger alert-dismissible" role="alert" v-for="error in errors" :key="error">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <i class="fa fa-times-circle"></i> {{ error }}
+      </div>
+    </div>
+    <div class="alert alert-danger alert-dismissible" role="alert" v-if="error">
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+      <i class="fa fa-times-circle"></i> {{ error }}
+    </div>
+    <div class="alert alert-success alert-dismissible" role="alert" v-if="success">
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+      <i class="fa fa-check-circle"></i> {{ success }}
+    </div>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
